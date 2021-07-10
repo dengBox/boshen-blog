4305d9a999479421e27fe2258bddf8394e87f260
dengbo <1716775221@qq.com>
2021-07-10 14:33:30 +0800

    初始化项目

---
+++ vuepress.config.js
@@ -0,0 +1,19 @@
+export default {
+  base: 'docs/',
+  // title: '',
+  // description: '',
+  head: [],
+  theme: 'antdocs',
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: 'VuePress',
+      description: '记录我的程生活',
+    },
+    '/zh/': {
+      lang: 'en-US',
+      title: 'VuePress',
+      description: 'Record my programming lifer',
+    }
+  }
+}
